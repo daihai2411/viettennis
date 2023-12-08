5d32bd0b79e1c23b7ef2f9a285e91a94953273eb
hai.nguyen <daihai241199@gmail.com>
2023-12-08 12:36:39 +0700

    update

--- src/components/header/menuTablet/index.tsx
+++ src/components/header/menuTablet/index.tsx
@@ -15,7 +15,7 @@ const MenuTablet = () => {
   const pathName = usePathname();
 
   return (
-    <NavbarMenu className="mt-[60px]">
+    <NavbarMenu className="mt-12">
       {headers.map((item) => (
         <>
           {item.children && item.disableClick ? (
@@ -24,7 +24,10 @@ const MenuTablet = () => {
                 title={<div className="py-0 text-lg">{item.label}</div>}
               >
                 {item.children.map((item1) => (
-                  <div key={item1.route} className="px-4 py-1 block !text-lg">
+                  <div
+                    key={item1.route}
+                    className="px-4 py-1 block !text-lg cursor-pointer"
+                  >
                     <Link
                       color="foreground"
                       className="w-full hover:text-green-common"
@@ -37,7 +40,7 @@ const MenuTablet = () => {
               </AccordionItem>
             </Accordion>
           ) : (
-            <NavbarMenuItem key={item.key} className="px-2 py-2">
+            <NavbarMenuItem key={item.key} className="px-2 py-2 cursor-pointer">
               <Link
                 style={{ color: pathName === item.route ? "#2DA46B" : "" }}
                 color="foreground"
