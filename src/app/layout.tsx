e55ed41e606383c6936cc600925f59926e2de659
hai.nguyen <daihai241199@gmail.com>
2023-12-19 21:02:37 +0700

    Add chat plugin messeger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { NextAuthProvider } from "./Providers";
 
 export default function RootLayout({
@@ -18,6 +19,40 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body suppressHydrationWarning={true}>
+        {/* <!-- Messenger Chat plugin Code --> */}
+        <div id="fb-root"></div>
+
+        {/* <!-- Your Chat plugin code --> */}
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Script
+          id="messenger-tag"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "171085149429527");
+            chatbox.setAttribute("attribution", "biz_inbox");`,
+          }}
+        ></Script>
+        <Script
+          id="messenger-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v18.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
+          }}
+        ></Script>
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
