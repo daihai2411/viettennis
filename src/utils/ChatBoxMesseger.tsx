25f938acdb84d9b5dd2df08bb545bada5513c631
hai.nguyen <daihai241199@gmail.com>
2023-12-20 10:03:49 +0700

    Fix UI chatbox, scroll top

--- src/utils/ChatBoxMesseger.tsx
+++ src/utils/ChatBoxMesseger.tsx
@@ -5,7 +5,7 @@ import { MessengerChat } from "react-messenger-chat-plugin";
 const ChatBoxMesseger = () => {
   return (
     <MessengerChat
-      pageId="171085149429527"
+      pageId="204082679699567"
       language="vi"
       themeColor={"#000000"}
       loggedInGreeting="loggedInGreeting"
