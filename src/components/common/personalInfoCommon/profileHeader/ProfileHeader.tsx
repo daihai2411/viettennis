14c78c7a7f6d9637cbf3d4dbe4f42bd6ed95d006
hai.nguyen <daihai241199@gmail.com>
2024-01-12 12:17:51 +0700

    Update UI

--- src/components/common/personalInfoCommon/profileHeader/ProfileHeader.tsx
+++ src/components/common/personalInfoCommon/profileHeader/ProfileHeader.tsx
@@ -6,7 +6,7 @@ const ProfileHeader = ({ data, loading, isUserLogged }) => {
   return (
     <>
       <div
-        className={`my-2 px-0 md:px-2 mx-2 md:mx-0 h-auto md:h-[520px] block md:flex ${style.bgTwoColorVertical}`}
+        className={`my-2 px-0 md:px-2 mx-2 md:mx-0 h-auto md:h-[530px] block md:flex ${style.bgTwoColorVertical}`}
       >
         <ProfileHeaderLeft
           data={data}
